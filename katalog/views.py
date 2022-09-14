4345002930e4cd8dfac0df4ace3ea1bbdc721730
mariana <marianasalma11@gmail.com>
2022-09-14 18:25:29 +0700

    Finish catalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Mariana Salma',
+        'npm' : '2106702516'
+    }
+    return render(request, "katalog.html", context)
