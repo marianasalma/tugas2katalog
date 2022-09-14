d372e8be47c841982cce133de7d08d14fc459e96
mariana <marianasalma11@gmail.com>
2022-09-14 22:52:32 +0700

    Add unit testing

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+# Function untuk melakukan testing terhadap url /katalog/
+class TestUrl(TestCase):
+    def test_url(self):
+        self.assertEqual(Client().get('/katalog/').status_code, 200)
