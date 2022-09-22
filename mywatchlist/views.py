add79d9cd09f246154ea2a8f281c46dda11e596c
mariana <marianasalma11@gmail.com>
2022-09-22 11:10:06 +0700

    Fix loaddata procfile

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -20,7 +20,3 @@ def show_xml(request):
 def show_json(request):
     data = WatchlistItem.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_json_by_id(request,id):
-    data = WatchlistItem.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
